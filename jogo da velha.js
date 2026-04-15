9d21cd69bce4f23a8040989f38ab75ca7a05aceb
thur0675 <arthur.desiderio.m@gmail.com>
2026-04-15 07:52:26 -0300

    mudança 5

--- jogo da velha.js
+++ jogo da velha.js
@@ -4,3 +4,7 @@ casas[4] = "O";
 function colocarsimbolo(posicao, simbolo) {
     casas[posicao] = simbolo;
 }
+colocarsimbolo(1, "X");
+colocarsimbolo(2, "X");
+colocarsimbolo(3, "O");
+console.log(casas);
